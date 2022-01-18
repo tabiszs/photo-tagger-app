b9702e9304032b75692b11fad57cf475472c0a6a
Stanislaw <92331225+tabiszs@users.noreply.github.com>
2022-01-18 22:17:22 +0100

    add scrollable login page

--- lib/pages/authenticate/sign_out_page.dart
+++ lib/pages/authenticate/sign_out_page.dart
@@ -4,14 +4,31 @@ import 'package:photo_tagger/pages/authenticate/auth_cubit.dart';
 import 'package:photo_tagger/pages/authenticate/auth_state.dart';
 import 'package:photo_tagger/pages/authenticate/microsoft/sign_in_button.dart';
 
-class SignOutPage extends StatefulWidget {
+class SignOutPage extends StatelessWidget {
   const SignOutPage({Key? key}) : super(key: key);
 
   @override
-  State<SignOutPage> createState() => _SignOutPageState();
+  Widget build(BuildContext context) {
+    return BlocBuilder<AuthCubit, AuthState>(builder: (context, state) {
+      return MaterialApp(home: LogInFormWidget(state: state));
+    });
+  }
 }
 
-class _SignOutPageState extends State<SignOutPage> {
+class LogInFormWidget extends StatefulWidget {
+  const LogInFormWidget({
+    Key? key,
+    required this.state,
+  }) : super(key: key);
+
+  final AuthState state;
+
+  @override
+  State<LogInFormWidget> createState() => _LogInFormWidgetState();
+}
+
+// https://stackoverflow.com/questions/46551268/when-the-keyboard-appears-the-flutter-widgets-resize-how-to-prevent-this
+class _LogInFormWidgetState extends State<LogInFormWidget> {
   final emailController = TextEditingController();
   final passwordController = TextEditingController();
   static const double _edgeInsets = 32;
@@ -19,68 +36,75 @@ class _SignOutPageState extends State<SignOutPage> {
 
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<AuthCubit, AuthState>(builder: (context, state) {
-      return MaterialApp(
-        home: Material(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: _edgeInsets),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                const SizedBox(height: 2 * _edgeInsets),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Image.asset(
-                    'assets/images/skauci-logo.png',
-                    height: 150,
+    AuthState state = widget.state;
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: _edgeInsets),
+        child: SingleChildScrollView(
+          child: ConstrainedBox(
+            constraints: BoxConstraints(
+              minWidth: MediaQuery.of(context).size.width,
+              minHeight: MediaQuery.of(context).size.height,
+            ),
+            child: IntrinsicHeight(
+              child: Column(
+                mainAxisSize: MainAxisSize.max,
+                children: <Widget>[
+                  const SizedBox(height: 2 * _edgeInsets),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Image.asset(
+                      'assets/images/skauci-logo.png',
+                      height: 150,
+                    ),
                   ),
-                ),
-                const SizedBox(height: 16),
-                TextField(
-                  keyboardType: TextInputType.emailAddress,
-                  textInputAction: TextInputAction.next,
-                  decoration: InputDecoration(
-                    border: const OutlineInputBorder(),
-                    hintText: 'Adres e-mail: @skauci-europy.pl',
-                    errorText: correctDomain == null
-                        ? null
-                        : (correctDomain! ? null : "Niepoprawna domena."),
-                  ),
-                  controller: emailController,
-                  onSubmitted: (String text) {
-                    setState(() {
-                      text.trimRight();
-                      correctDomain = text.endsWith('@skauci-europy.pl');
-                    });
-                  },
-                ),
-                const SizedBox(height: 16),
-                TextField(
-                  obscureText: true,
-                  autocorrect: false,
-                  enableSuggestions: false,
-                  textInputAction: TextInputAction.done,
-                  decoration: const InputDecoration(
-                    border: OutlineInputBorder(),
-                    hintText: 'Hasło',
-                  ),
-                  controller: passwordController,
-                ),
-                const SizedBox(height: 16),
-                if (state is SignedOutState && state.error != null) ...[
-                  Text(state.error!),
                   const SizedBox(height: 16),
-                ] else
-                  const SizedBox(height: 32),
-                SignInButton(
-                  email: emailController,
-                  password: passwordController,
-                )
-              ],
+                  TextField(
+                    keyboardType: TextInputType.emailAddress,
+                    textInputAction: TextInputAction.next,
+                    decoration: InputDecoration(
+                      border: const OutlineInputBorder(),
+                      hintText: 'Adres e-mail: @skauci-europy.pl',
+                      errorText: correctDomain == null
+                          ? null
+                          : (correctDomain! ? null : "Niepoprawna domena."),
+                    ),
+                    controller: emailController,
+                    onSubmitted: (String text) {
+                      setState(() {
+                        text.trimRight();
+                        correctDomain = text.endsWith('@skauci-europy.pl');
+                      });
+                    },
+                  ),
+                  const SizedBox(height: 16),
+                  TextField(
+                    obscureText: true,
+                    autocorrect: false,
+                    enableSuggestions: false,
+                    textInputAction: TextInputAction.done,
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: 'Hasło',
+                    ),
+                    controller: passwordController,
+                  ),
+                  const SizedBox(height: 16),
+                  if (state is SignedOutState && state.error != null) ...[
+                    Text(state.error!),
+                    const SizedBox(height: 16),
+                  ] else
+                    const SizedBox(height: 32),
+                  SignInButton(
+                    email: emailController,
+                    password: passwordController,
+                  )
+                ],
+              ),
             ),
           ),
         ),
-      );
-    });
+      ),
+    );
   }
 }
